bef4ef73f3580de74237c60bb0fb6cf211042ab2
Sankalp Mukim <sankalpmukim@gmail.com>
2023-07-11 11:17:20 +0530

    first draft

---
+++ src/components/Content.tsx
@@ -0,0 +1,103 @@
+import {
+  CheckCircleIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/20/solid";
+import {
+  CONCLUSION_HEADING,
+  CONCLUSION_P1,
+  CONCLUSION_P2,
+  CONCLUSION_P3,
+  HOW_IMPROVE,
+  HOW_IMPROVE_TEXT,
+  INTRO,
+  LIST_POINTS_IMPROVE,
+  LIST_POINTS_NOISE,
+  TITLE,
+  TOP_TITLE,
+  WHEN_HEADING,
+  WHEN_NOISE_POST_TEXT,
+  WHEN_NOISE_TEXT,
+  WHY_ANSWER,
+  WHY_QUESTION,
+} from "~/components/constants";
+import { ReactNode } from "react";
+
+const SubHeading = ({ children }: { children: ReactNode }) => (
+  <p className="text-base font-semibold leading-7 text-indigo-600">
+    {children}
+  </p>
+);
+
+const MainHeading = ({ children }: { children: ReactNode }) => (
+  <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+    {children}
+  </h1>
+);
+
+const MainHeading2 = ({ children }: { children: ReactNode }) => (
+  <h2 className="mt-6 text-2xl font-bold tracking-tight text-gray-900">
+    {children}
+  </h2>
+);
+
+const SubText = ({ children }: { children: ReactNode }) => <p>{children}</p>;
+const MainText = ({ children }: { children: ReactNode }) => (
+  <p className="mt-6 text-xl leading-8">{children}</p>
+);
+
+const AsideText = ({ children }: { children: ReactNode }) => (
+  <figure className="mt-10 border-l border-indigo-600 pl-9">
+    <blockquote className="font-semibold text-gray-900">
+      <p>{children}</p>
+    </blockquote>
+  </figure>
+);
+
+export interface ListPointsType {
+  content: string;
+  title: string;
+}
+
+const ListPoints = ({ points }: { points: ListPointsType[] }) => (
+  <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
+    {points.map(({ title, content }, index) => (
+      <li className="mt-0 flex gap-x-3" key={index}>
+        <CheckCircleIcon
+          className="mt-1 h-5 w-5 flex-none text-indigo-600"
+          aria-hidden="true"
+        />
+        <span>
+          <strong className="font-semibold text-gray-900">{title}</strong>
+
+          <div>{content}</div>
+        </span>
+      </li>
+    ))}
+  </ul>
+);
+export default function Content() {
+  return (
+    <div className="bg-white px-6 py-32 lg:px-8">
+      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <SubHeading>{TOP_TITLE}</SubHeading>
+        <MainHeading>{TITLE}</MainHeading>
+        <div className="mt-10 max-w-2xl">
+          <SubText>{INTRO}</SubText>
+          <MainHeading2>{WHY_QUESTION}</MainHeading2>
+          <MainText>{WHY_ANSWER}</MainText>
+          <MainHeading2>{WHEN_HEADING}</MainHeading2>
+          <MainText>{WHEN_NOISE_TEXT}</MainText>
+          <ListPoints points={LIST_POINTS_NOISE} />
+          <p className="mt-8">{WHEN_NOISE_POST_TEXT}</p>
+          <MainHeading2>{HOW_IMPROVE}</MainHeading2>
+          <MainText>{HOW_IMPROVE_TEXT}</MainText>
+          <ListPoints points={LIST_POINTS_IMPROVE} />
+          <MainHeading2>{CONCLUSION_HEADING}</MainHeading2>
+          <MainText>{CONCLUSION_P1}</MainText>
+          <MainText>{CONCLUSION_P2}</MainText>
+          <AsideText>{CONCLUSION_P3}</AsideText>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/constants.ts
@@ -0,0 +1,61 @@
+import { ListPointsType } from "~/components/Content";
+
+export const TOP_TITLE = `So sorry but`;
+export const TITLE = `Your opinion was discarded.`;
+export const INTRO = `We understand that everybody has an opinion, and each of those opinions is valuable in its own right. But here's the thing - opinions without proper foundation or knowledge can do more harm than good.`;
+export const WHY_QUESTION = `Why was your opinion discarded?`;
+export const WHY_ANSWER = `You've been directed to this page because your response or opinion, as it currently stands, lacks proper substance, context, or relevancy. It's not about silencing you or negating your voice. It's about fostering a conversation that's thoughtful, productive, and based on well-informed perspectives.`;
+export const WHEN_HEADING = `When An Opinion Becomes Noise`;
+export const WHEN_NOISE_TEXT = `Opinions transform into noise when they:`;
+export const LIST_POINTS_NOISE: ListPointsType[] = [
+  {
+    title: `Outdated`,
+    content: `Are based on outdated or irrelevant sources.`,
+  },
+  {
+    content: `Encourage a fallacious argument.`,
+    title: `Fallacious`,
+  },
+  {
+    content: `Fail to consider the nuances of the present context.`,
+    title: `Ignorant`,
+  },
+  {
+    content: `Are disrespectful, derogatory, or offensive.`,
+    title: `Disrespectful`,
+  },
+  {
+    content: `Focus more on provocation rather than fruitful conversation.`,
+    title: `Provocative`,
+  },
+];
+export const WHEN_NOISE_POST_TEXT = `In the era of 'outrage culture,' it's crucial to step back and evaluate if our opinions contribute value to the discussion or simply ignite unnecessary flame wars.`;
+export const HOW_IMPROVE = `How To Build A Worthwhile Opinion`;
+export const HOW_IMPROVE_TEXT = `There are a few things that one can do to ensure their opinion is not just heard but respected and valued:`;
+export const LIST_POINTS_IMPROVE: ListPointsType[] = [
+  {
+    title: `Stay Updated`,
+    content: `Ensure your opinion is based on the most recent and relevant information available.`,
+  },
+  {
+    title: `Critical Thinking`,
+    content: `Analyze the information you have before forming an opinion. Avoid bandwagon thinking and knee-jerk reactions.`,
+  },
+  {
+    title: `Open-Mindedness`,
+    content: `Be willing to consider other perspectives and adjust your view as necessary.`,
+  },
+  {
+    title: `Respectful Communication`,
+    content: `A well-articulated opinion loses its value when wrapped in offensive or disrespectful language.`,
+  },
+  {
+    title: `Learn and Grow`,
+    content: `The best opinions are those that evolve with new insights and information. Accept that you might be wrong and be willing to grow.`,
+  },
+];
+
+export const CONCLUSION_HEADING = `Let's Elevate the Conversation`;
+export const CONCLUSION_P1 = `It's time to move past anger and outrage, past the knee-jerk reactions and trolling. It's time to promote productive dialogue, intellectual growth, and respectful discourse.`;
+export const CONCLUSION_P2 = `This site isn't a punishment but a chance to learn. So, the next time you share your opinion, remember:`;
+export const CONCLUSION_P3 = `It's not about being right. It's about being relevant and respectful.`;
